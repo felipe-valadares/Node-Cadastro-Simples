bfbd677a2ca5a628319c2f5ace9dd5128223c129
Felipe Valadares <agapitonogueira@gmail.com>
2024-01-04 17:33:22 -0300

    identificação de parametros e novas funções

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,22 @@
 import { Database } from "./database.js";
 import { randomUUID } from 'node:crypto';
-
+import { buildRoutePath } from "./utils/build-route-path.js";
+import path from "node:path";
+import { json } from "./middlewares/json.js";
 
 const database = new Database();
 
 export const routes = [
     {
         method: 'GET',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
-            const users = database.select('users');
+            const { search } = req.query;
+
+            const users = database.select('users', search ? {
+                name: search,
+                email: search
+            } : null);
 
             return res.end(JSON.stringify(users))
         }
@@ -17,7 +24,7 @@ export const routes = [
 
     {
         method: 'POST',
-        path: '/users',
+        path: buildRoutePath('/users'),
         handler: (req, res) => {
             const { name, email } = req.body;
 
@@ -31,6 +38,34 @@ export const routes = [
 
             return res.writeHead(201).end();
         }
-    }
+    },
 
+    {
+        method: 'PUT',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params;
+
+            const { name, email } = req.body;
+
+            database.update('users', id, {
+                name,
+                email
+            });
+
+            return res.writeHead(204).end()
+        }
+    },
+
+    {
+        method: 'DELETE',
+        path: buildRoutePath('/users/:id'),
+        handler: (req, res) => {
+            const { id } = req.params;
+
+            database.delete('users', id);
+
+            return res.writeHead(204).end()
+        }
+    },
 ]
